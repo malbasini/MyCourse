1d954879162c94977c884652a660d1f11837f532
malbasini <malbasini@gmail.com>
2020-06-06 16:39:11 +0200

    Gestione db con EF

--- MyCourse/Startup.cs
+++ MyCourse/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyCourse.Models.Services.Application;
 using MyCourse.Models.Services.Infrastucture;
@@ -19,8 +20,13 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddTransient<ICourseService, AdoNetCourseService>();
             services.AddTransient<IDatabaseAccessor, SqliteDatabaseAccessor>();
+            //services.AddTransientCourseService, AdoNetCourseService>();
+            services.AddTransient<ICourseService, EFCoreCourseService>();
+            services.AddTransient<IDatabaseAccessor, SqliteDatabaseAccessor>();
+            services.AddDbContextPool<MyCourseDbContext>(optionsBuilder => {
+                optionsBuilder.UseSqlite("Data Source = Data/MyCourse.db");  
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
