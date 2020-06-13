51d62bb3627e6c6247e5365fad2102150f30d9f0
malbasini <malbasini@gmail.com>
2020-06-13 18:52:24 +0200

    Creato partial view

--- MyCourse/Customizations/ViewComponents/PaginationBarViewComponent.cs
+++ MyCourse/Customizations/ViewComponents/PaginationBarViewComponent.cs
@@ -8,7 +8,8 @@ namespace MyCourse.Customizations.ViewComponents
         //Ci serve il numero di pagina corrente
         //Il numero totale dei risultati
         //Il numero di risultati per pagina
-        public IViewComponentResult Invoke(CourseListViewModel model)
+        //Search OrderBy e Ascending
+        public IViewComponentResult Invoke(IPaginationInfo model)
         {
             return View(model);
         }
