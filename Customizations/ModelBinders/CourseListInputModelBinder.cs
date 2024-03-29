1e91c332500f23ff6c6aca0c8585977518f4e58c
malbasini <malbasini@gmail.com>
2024-03-29 05:49:17 +0100

    Comment

--- Customizations/ModelBinders/CourseListInputModelBinder.cs
+++ Customizations/ModelBinders/CourseListInputModelBinder.cs
@@ -14,6 +14,21 @@ namespace MyCourse.Customizations.ModelBinders
         {
             this.coursesOptions = coursesOptions;
         }
+        /*--
+         Il metodo "BindModelAsync" è definito nella classe "CourseListInputModelBinder" che implementa 
+         l'interfaccia "IModelBinder". Questa classe è uno strumento di associazione di modelli personalizzato, 
+         utilizzato da ASP.NET Core per associare i dati della richiesta HTTP ai parametri delle Action.
+         In questo metodo, `bindingContext.ValueProvider` fornisce valori dalla richiesta HTTP. 
+         I dati della richiesta vengono recuperati e trasformati in tipologie adeguate. Quindi, 
+         con questi dati viene creata una nuova istanza di `CourseListInputModel`. Questa nuova istanza viene 
+         quindi impostata come risultato per il contesto di associazione per indicare che il modello è stato 
+         associato correttamente. Infine, il metodo restituisce "Task.CompletedTask" perché è un metodo asincrono, 
+         ma in questo caso non viene eseguito alcun lavoro asincrono effettivo.
+         Infine, questo raccoglitore di modelli personalizzati viene utilizzato nella classe 
+         "CourseListInputModel" utilizzando l'attributo "ModelBinder".
+         Pertanto, quando ASP.NET Core rileva un parametro di tipo "CourseListInputModel" in una Action, 
+         utilizzerà "CourseListInputModelBinder" per associare i dati della richiesta HTTP a tale parametro.
+         */
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
             //Recuperiamo i valori grazie ai value provider
