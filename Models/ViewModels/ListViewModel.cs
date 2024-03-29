647ccb6b1869c9500b3c359af593188c303f760b
malbasini <malbasini@gmail.com>
2024-03-29 04:40:33 +0100

    Final commit

--- Models/ViewModels/ListViewModel.cs
+++ Models/ViewModels/ListViewModel.cs
@@ -4,6 +4,9 @@ namespace MyCourse.Models.ViewModels
 {
     public class ListViewModel<T>
     {
+        /*--Result contiene al massimo 10 corsi, ed è quello che già
+         ci stava restituendo, in TotalCount ci andranno il numero totale
+         dei corsi corrispondenti alla ricerca dell'utente.*/
         public List<T> Results { get; set; }
         public int TotalCount { get; set; }
     }
